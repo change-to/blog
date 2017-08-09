9c13339a6420d2c930cf3ecb57266730797e7eb5
BabaShun <you@example.com>
2017-08-09 13:18:05 +0900

    fix nav

--- wp-content/themes/xeory_extension-child/header.php
+++ wp-content/themes/xeory_extension-child/header.php
@@ -168,16 +168,16 @@ wp_head();
 
 <?php } ?>
 <script>
-(function($) {
-    $(function() {
-        var $header = $('#header');
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 50) {
-                $header.addClass('fixed');
-            } else {
-                $header.removeClass('fixed');
-            }
-        });
-    });
-})(jQuery);
+// (function($) {
+//     $(function() {
+//         var $header = $('#header');
+//         $(window).scroll(function() {
+//             if ($(window).scrollTop() > 0) {
+//                 $header.addClass('fixed');
+//               }else{
+//                 $header.removeClass('fixed');
+//               }
+//         });
+//     });
+// })(jQuery);
 </script>
